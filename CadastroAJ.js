e4460be539c6b913ae281ec11dfc3de47c156e5f
anajuliif <anafeereira125@gmail.com>
2023-05-03 09:08:41 -0300

    mud

--- CadastroAJ.js
+++ CadastroAJ.js
@@ -6,11 +6,19 @@ submit.addEventListener('click', function(e){
     console.log('funcionou')
 })
 var j = {
-    'nomecomp' : ['Ana Julia Ferreira', 'Wellington Oliveira', 'Caike Henry', 'Caio Felipe'],
-    'cpf' : ['123.321.123.32', '345.543.345.54', '456.654.456.65', '567.765.567.76'],
-    'telefone' : ['1912321212', '1923432343', '1934543454', '4565456565'],
-    'email' : ['anajulia2023@gmail.com', 'wellington20232gmail.com', 'caikehenry2023@gmail.com', 'caiofelipe2023@gmail.com'],
-    'senha' : ['1234', '2345', '4567', '5678'],
-    'confsenha' : ['1234', '2345', '4567', '5678']
+    'nomecomp' : ['Ana Julia Ferreira'],
+    'cpf' : ['123.321.123.32'],
+    'telefone' : ['1912321212'],
+    'email' : ['anajulia2023@gmail.com'],
+    'senha' : ['1234'],
+    'confsenha' : ['1234']
 
 }
+for(let i=0; i< j.nomecomp.length; i++){
+    if (senha== j.senha){
+        window.alert("Seu cadastro foi concluído");
+    }else{
+        window.alert("Senha inválida")
+    }
+    }
+
