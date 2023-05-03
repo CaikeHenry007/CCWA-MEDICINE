0872aea44dda48965235b42117b07162950d145c
CaioFelps007 <125682381+CaioFelps007@users.noreply.github.com>
2023-05-03 10:56:02 -0300

    Add files via upload

--- LoginCA.js
+++ LoginCA.js
@@ -1,4 +1,23 @@
 
+<<<<<<< HEAD
+var Email;
+var Senha;
+var resultado;
+var mensagem;
+
+function Prosseguir() {
+    var Email = document.getElementById('Email').value;
+    var Senha = document.getElementById('Senha').value;
+
+    if (Email == "usuario@gmail.com" && Senha == "2345") {
+        alert('Email e senha corretos!')
+        location.href = ""
+    } else {
+        alert("Usuário e/ou senha incorretos");
+    }
+}
+
+=======
 function login(){
     var login = document.getElementById('login').value;
     var senha = document.getElementById('senha').value;
@@ -11,3 +30,4 @@ alert('Usuário ou senha incorretos');
     }
     }
     
+>>>>>>> 681b1d4233d6e75b261dec3604d2ea18e2c5a9a0
