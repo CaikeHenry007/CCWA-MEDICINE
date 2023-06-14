e4defb8a2dae2bf0600c17202a45769fe056cacd
PauloPZ <wellington.gts21@gmail.com>
2023-06-14 14:51:38 -0300

    Pronto

--- Login/Login.js
+++ Login/Login.js
@@ -1,13 +1,26 @@
-
-function Prosseguir(){
-    var Email = document.getElementById('Email').value;
-    var Senha = document.getElementById('Senha').value;
-
-    if(Email == "anajulia2023@gmail.com" && Senha == "12345"){
-        alert('Você foi logado com sucesso');
-        location.href = "../InícioLogado/InícioLogado.html";
-    }else{
-alert('Usuário ou senha incorretos');
-    }
-    }
+function Funcao(){event.preventDefault;
+    var escolher = document.getElementById('escolher').value;
+    var email = document.getElementById('email').value;
+    var senha = document.getElementById('senha').value;
     
+
+    /*Paciente*/
+    if(email == "ferreira@gmail.com" && senha == "12345678" && escolher == "Paciente") {
+        alert('Você foi logado com sucesso! Redirecionando para Página Inicial...');
+        location.href = "../InícioLogado/InícioLogado.html";
+    }
+
+    /*Médico*/
+    if(email == "caike@gmail.com" && senha == "123456" && escolher == "Médico") {
+        alert('Você foi logado com sucesso! Redirecionando para Médico');
+        location.href = "../Caio/MÉDICO.html";
+    }
+
+    /*Admin*/
+    if(email == "wellington@gmail.com" && senha == "1234" && escolher == "Administrador") {
+        alert('Você foi logado com sucesso! Redirecionando para Admin...');
+        location.href = "../Caio/AGENDAMENTO.html";
+    } else {
+        alert("Email ou senha inválidos");
+    }
+}
